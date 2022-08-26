3f2206d44be285ebc7cfba88aa079c5d3e1899f3
Ronaldo Pace <ronaldopace@gmail.com>
2022-08-26 15:57:35 +0200

    Added publishing code

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 https://xkcd.com/303/
 
 ![compiling](https://imgs.xkcd.com/comics/compiling.png)
+
+## How to use
+
+Just apply it to your root project and your total sitting around time will be recorded in a `slowMachine.properties`
+file.
+
+PS.: Don't forget to add this file to the your `.gitignore`
+
+```kotlin
+plugins {
+    id("io.github.budius.slow-machine")
+}
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.BufferedReader
+import java.io.FileReader
+import java.util.*
 
 plugins {
     kotlin("jvm") version "1.7.10"
     id("java-gradle-plugin")
+    id("maven-publish")
+    id("com.gradle.plugin-publish") version "1.0.0"
 }
 
-group = "com.budius"
+group = "io.github.budius"
 version = "0.0.1"
 
 repositories {
@@ -16,14 +21,35 @@ dependencies {
 
 }
 
+val local = Properties().apply {
+    val file = File(rootProject.rootDir, "local.properties")
+    if (file.exists()) {
+        BufferedReader(FileReader(file)).use { load(it) }
+    }
+}
+
+project.ext["gradle.publish.key"] = local["gradle.publish.key"]
+project.ext["gradle.publish.secret"] = local["gradle.publish.secret"]
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+pluginBundle {
+    website = "https://github.com/budius/slowGradleMachine"
+    vcsUrl = "https://github.com/budius/slowGradleMachine.git"
+    tags = listOf("performance", "speed", "slow", "time", "tracker")
+}
+
 gradlePlugin {
     plugins {
-        create("slowGradleMachine") {
-            id = "com.budius.slow-gradle-machine"
+        create("slowMachine") {
+            id = "${group}.slow-machine"
+            displayName = "Is my computer slow?"
+            description = "Inspired by a real life scenario, this small utility tracks how long " +
+                    "you've been sitting around and waiting for your computer to compile. " +
+                    "It saves in a simple properties format and could be helpful to make the IT department" +
+                    "to get you a new computer with faster processor and more RAM."
             implementationClass = "com.budius.SlowGradleMachine"
         }
     }
